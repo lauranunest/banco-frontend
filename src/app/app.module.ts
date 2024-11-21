b9a80b42a43957ea433a1cfb7d5f8f5a671faa86
Laura Nunes <lauranunest@hotmail.com>
2024-11-21 18:42:26 -0300

    feat: página de cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CadastroComponent } from "./cadastro/cadastro.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CadastroComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+
+@Component({
+  selector: "app-cadastro",
+  templateUrl: "./cadastro.component.html",
+  styleUrls: ["./cadastro.component.scss"],
+})
+export class CadastroComponent implements OnInit {
+  cadastroForm: FormGroup;
+  mostrarErro: boolean = false;
+  mostrarSucesso: boolean = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.cadastroForm = this.fb.group({
+      numeroConta: "",
+      numeroAgencia: "",
+      nome: "",
+      rg: "",
+      endereco: "",
+      email: "",
+    });
+  }
+
+  submeter() {
+    if (this.cadastroForm.valid) {
+      localStorage.setItem("user", JSON.stringify(this.cadastroForm.value));
+      this.mostrarErro = false;
+      this.mostrarSucesso = true;
+    } else {
+      this.mostrarErro = true;
+      this.mostrarSucesso = false;
+    }
+  }
+
+  somenteNumeros(event: any) {
+    event.target.value = event.target.value.replace(/[^0-9]/g, "");
+  }
+
+  somenteLetras(event: any) {
+    event.target.value = event.target.value.replace(
+      /[^a-zA-ZáÁéÉíÍóÓúÚãÃçÇâÂêÊîÎôÔûÛàÀ]/g,
+      ""
+    );
+  }
+}
